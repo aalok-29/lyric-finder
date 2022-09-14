bf921a7486e2be5b7bd7847da58946afc2c261b2
aalok-29 <aaloknautiyal02@gmail.com>
2022-09-14 20:47:48 +0530

    commit

--- src/components/lyrics.js
+++ src/components/lyrics.js
@@ -17,11 +17,15 @@ function Lyrics() {
         //function to frtch data from API
         function getlyrics()
         {
+//             fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${track}&q_artist=${artist}&apikey=1538e2542ebac4878de127b5193d9854`).then((res) => {
+//     setLyrics(res.data.message.body.lyrics.lyrics_body);
+    
+//   })
             axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${track}&q_artist=${artist}&apikey=1538e2542ebac4878de127b5193d9854`).then(res => {
            setLyrics(res.data.message.body.lyrics.lyrics_body);
         })
         .catch(err => {console.log(err);setLyrics("An ERROR occured please try again")})
-        console.log("done")
+        // console.log("done")
         }
         //to check if user has searched for a new lyrics
         if(searchState===true)
