cebe8b0facae0d4831d7a9499641fa9b648bf3c9
aalok-29 <aaloknautiyal02@gmail.com>
2022-09-14 22:56:35 +0530

    bypass proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const proxy = require("http-proxy-middleware");
+
+module.exports = function(app) {
+  app.use(
+    proxy(`/matcher.lyrics.get?q_track=${track}&q_artist=${artist}&apikey=1538e2542ebac4878de127b5193d9854`, {
+      target: "https://api.musixmatch.com/ws/1.1",
+      secure: false,
+      changeOrigin: true
+    })
+  );
+};
